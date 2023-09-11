0a9f15bb26f5843b8e311ecd8eea758ee4c6653b
govind <govind.kewat@newtechfusion.com>
2023-09-11 11:37:53 +0530

    init repo

---
+++ Number_systm/My-increment/Myincrement.java
@@ -0,0 +1,15 @@
+public class Myincrement {
+   public static void main(String[]args){
+    int a = 30;
+    int b = a++;
+    int c = ++a;
+    int d = a--;
+    int e = --a;
+    System.out.println("b=>"+b);
+    System.out.println("c=>"+c);
+    System.out.println("d=>"+d);
+    System.out.println("e=>"+e);
+
+
+   }
+}
