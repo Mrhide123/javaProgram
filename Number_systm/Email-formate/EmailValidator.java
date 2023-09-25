35fe02bc0b346dede98beaa6d83be772db7d664e
Mrhide123 <77138606+Mrhide123@users.noreply.github.com>
2023-09-25 20:10:42 +0530

    without any packge

--- Number_systm/Email-formate/EmailValidator.java
+++ Number_systm/Email-formate/EmailValidator.java
@@ -1,14 +1,6 @@
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class EmailValidator {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an email address: ");
-        String email = scanner.nextLine();
-        scanner.close();
+        String email = "example@email.com"; // Replace with the email you want to validate
 
         if (isValidEmail(email)) {
             System.out.println("Valid email address.");
@@ -18,9 +10,6 @@ public class EmailValidator {
     }
 
     public static boolean isValidEmail(String email) {
-        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+        return email != null && email.contains("@");
     }
 }
